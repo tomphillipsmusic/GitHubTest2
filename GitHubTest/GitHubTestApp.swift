5295a51855f2b8221d820a449a1f4e86ccab488a
Tom Phillips <tomphillipsmusic@gmail.com>
2022-12-12 16:54:23 -0500

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Tom Phillips on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
